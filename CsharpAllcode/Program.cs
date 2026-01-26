d79fcb132e9bd160e7c63c32b12d1997d52157d6
sajib <sajibg563@gmail.com>
2026-01-26 15:45:21 +0600

    Add project files.

---
+++ CsharpAllcode/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CsharpAllcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10;
+            //     a = 20;
+            //Console.WriteLine(a);
+
+            //int a = 10;// Implicit
+            //double b = a;
+            //Console.WriteLine(b);
+
+            double x = 10.50;
+            int y = (int)x;
+            Console.WriteLine(y);
+
+            // user input
+
+
+
+           // Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ userinput/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace userinput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("enter your age : ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("your age is : " +age);
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine()!;
+
+            Console.WriteLine("Hello " + name);
+
+
+        }
+    }
+}
