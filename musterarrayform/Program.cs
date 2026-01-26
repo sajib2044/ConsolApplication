968d268290961e470919179e242e32bef619caa6
sajib <sajibg563@gmail.com>
2026-01-27 01:50:41 +0600

    master array

---
+++ musterarrayform/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace musterarrayform
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = 10;
+                int y = 0;
+
+                Console.WriteLine(x / y);
+            }
+            catch {
+                Console.WriteLine("Cannot divide by zero");
+            }
+        }
+    }
+}
