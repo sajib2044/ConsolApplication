b8a42892fc7d23e9f6869d4eb9beb29bcb3f3542
Sajib Ghosh <sajibg563@gmail.com>
2026-01-30 22:30:55 +0600

    Update Program.cs
    added my name

--- array/Program.cs
+++ array/Program.cs
@@ -42,6 +42,8 @@
             //    Console.WriteLine(name);
             //}
 
+            Console.WriteLine("sajib");
+
             int[] ids = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
 
             foreach (int id in ids) {
